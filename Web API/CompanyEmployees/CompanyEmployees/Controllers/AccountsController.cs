cd657fb24ecc88907064ae048e3c6406967420f7
Code Maze <contact@code-maze.com>
2022-05-17 11:48:16 +0200

    Update to .NET 6 and Angular 13

---
+++ Web API/CompanyEmployees/CompanyEmployees/Controllers/AccountsController.cs
@@ -0,0 +1,198 @@
+﻿using AutoMapper;
+using CompanyEmployees.Entities.DataTransferObjects;
+using CompanyEmployees.Entities.Models;
+using CompanyEmployees.JwtFeatures;
+using EmailService;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace CompanyEmployees.Controllers
+{
+    [Route("api/accounts")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IMapper _mapper;
+        private readonly JwtHandler _jwtHandler;
+        private readonly IEmailSender _emailSender;
+
+        public AccountsController(UserManager<User> userManager, IMapper mapper, JwtHandler jwtHandler,
+            IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+            _jwtHandler = jwtHandler;
+            _emailSender = emailSender;
+        }
+
+        [HttpPost("Registration")]
+        public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration)
+        {
+            if (userForRegistration == null || !ModelState.IsValid)
+                return BadRequest();
+
+            var user = _mapper.Map<User>(userForRegistration);
+            var result = await _userManager.CreateAsync(user, userForRegistration.Password);
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(e => e.Description);
+
+                return BadRequest(new RegistrationResponseDto { Errors = errors });
+            }
+
+            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            var param = new Dictionary<string, string?>
+            {
+                {"token", token },
+                {"email", user.Email }
+            };
+
+            var callback = QueryHelpers.AddQueryString(userForRegistration.ClientURI, param);
+            
+            var message = new Message(new string[] { user.Email }, "Email Confirmation token", callback, null);
+            await _emailSender.SendEmailAsync(message);
+
+            await _userManager.AddToRoleAsync(user, "Viewer");
+
+            return StatusCode(201);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] UserForAuthenticationDto userForAuthentication)
+        {
+            var user = await _userManager.FindByNameAsync(userForAuthentication.Email);
+            if (user == null)
+                return BadRequest("Invalid Request");
+
+            if (!await _userManager.IsEmailConfirmedAsync(user))
+                return Unauthorized(new AuthResponseDto { ErrorMessage = "Email is not confirmed" });
+
+            if (!await _userManager.CheckPasswordAsync(user, userForAuthentication.Password))
+            {
+                await _userManager.AccessFailedAsync(user);
+
+                if (await _userManager.IsLockedOutAsync(user))
+                {
+                    var content = $@"Your account is locked out. To reset the password click this link: {userForAuthentication.ClientURI}";
+                    var message = new Message(new string[] { userForAuthentication.Email }, 
+                        "Locked out account information", content, null);
+                    
+                    await _emailSender.SendEmailAsync(message);
+
+                    return Unauthorized(new AuthResponseDto { ErrorMessage = "The account is locked out" });
+                }
+
+                return Unauthorized(new AuthResponseDto { ErrorMessage = "Invalid Authentication" });
+            }
+
+            if (await _userManager.GetTwoFactorEnabledAsync(user))
+                return await GenerateOTPFor2StepVerification(user);
+
+            var token = await _jwtHandler.GenerateToken(user);
+
+            await _userManager.ResetAccessFailedCountAsync(user);
+
+            return Ok(new AuthResponseDto { IsAuthSuccessful = true, Token = token });
+        }
+
+        private async Task<IActionResult> GenerateOTPFor2StepVerification(User user)
+        {
+            var providers = await _userManager.GetValidTwoFactorProvidersAsync(user);
+            if (!providers.Contains("Email"))
+            {
+                return Unauthorized(new AuthResponseDto { ErrorMessage = "Invalid 2-Step Verification Provider." });
+            }
+
+            var token = await _userManager.GenerateTwoFactorTokenAsync(user, "Email");
+            var message = new Message(new string[] { user.Email }, "Authentication token", token, null);
+            
+            await _emailSender.SendEmailAsync(message);
+            
+            return Ok(new AuthResponseDto { Is2StepVerificationRequired = true, Provider = "Email" });
+        }
+
+        [HttpPost("ForgotPassword")]
+        public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordDto forgotPasswordDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var user = await _userManager.FindByEmailAsync(forgotPasswordDto.Email);
+            if (user == null)
+                return BadRequest("Invalid Request");
+
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var param = new Dictionary<string, string?>
+            {
+                {"token", token },
+                {"email", forgotPasswordDto.Email }
+            };
+
+            var callback = QueryHelpers.AddQueryString(forgotPasswordDto.ClientURI, param);
+            var message = new Message(new string[] { user.Email }, "Reset password token", callback, null);
+
+            await _emailSender.SendEmailAsync(message);
+            
+            return Ok();
+        }
+
+        [HttpPost("ResetPassword")]
+        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordDto resetPasswordDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var user = await _userManager.FindByEmailAsync(resetPasswordDto.Email);
+            if (user == null)
+                return BadRequest("Invalid Request");
+            
+            var resetPassResult = await _userManager.ResetPasswordAsync(user, resetPasswordDto.Token, resetPasswordDto.Password);
+            if (!resetPassResult.Succeeded)
+            {
+                var errors = resetPassResult.Errors.Select(e => e.Description);
+                
+                return BadRequest(new { Errors = errors });
+            }
+
+            await _userManager.SetLockoutEndDateAsync(user, new DateTime(2000, 1, 1));
+            
+            return Ok();
+        }
+
+        [HttpGet("EmailConfirmation")]
+        public async Task<IActionResult> EmailConfirmation([FromQuery] string email, [FromQuery] string token)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+                return BadRequest("Invalid Email Confirmation Request");
+
+            var confirmResult = await _userManager.ConfirmEmailAsync(user, token);
+            if (!confirmResult.Succeeded)
+                return BadRequest("Invalid Email Confirmation Request");
+            
+            return Ok();
+        }
+
+        [HttpPost("TwoStepVerification")]
+        public async Task<IActionResult> TwoStepVerification([FromBody] TwoFactorDto twoFactorDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var user = await _userManager.FindByEmailAsync(twoFactorDto.Email);
+            if (user is null)
+                return BadRequest("Invalid Request");
+            
+            var validVerification = await _userManager.VerifyTwoFactorTokenAsync(user, twoFactorDto.Provider, twoFactorDto.Token);
+            if (!validVerification)
+                return BadRequest("Invalid Token Verification");
+            
+            var token = await _jwtHandler.GenerateToken(user);
+            
+            return Ok(new AuthResponseDto { IsAuthSuccessful = true, Token = token });
+        }
+    }
+}
---
+++ Web API/CompanyEmployees/CompanyEmployees/JwtFeatures/JwtHandler.cs
@@ -0,0 +1,69 @@
+﻿using CompanyEmployees.Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CompanyEmployees.JwtFeatures
+{
+    public class JwtHandler
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IConfigurationSection _jwtSettings;
+        private readonly UserManager<User> _userManager;
+        
+        public JwtHandler(IConfiguration configuration, UserManager<User> userManager)
+        {
+            _configuration = configuration;
+            _jwtSettings = _configuration.GetSection("JwtSettings");
+            _userManager = userManager;
+        }
+
+        public async Task<string> GenerateToken(User user)
+        {
+            var signingCredentials = GetSigningCredentials();
+            var claims = await GetClaims(user);
+            var tokenOptions = GenerateTokenOptions(signingCredentials, claims);
+            var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+            return token;
+        }
+
+        private SigningCredentials GetSigningCredentials()
+        {
+            var key = Encoding.UTF8.GetBytes(_jwtSettings.GetSection("securityKey").Value);
+            var secret = new SymmetricSecurityKey(key);
+
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+
+        private async Task<List<Claim>> GetClaims(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Email)
+            };
+
+            var roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            return claims;
+        }
+
+        private JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, List<Claim> claims)
+        {
+            var tokenOptions = new JwtSecurityToken(
+                issuer: _jwtSettings["validIssuer"],
+                audience: _jwtSettings["validAudience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(_jwtSettings["expiryInMinutes"])),
+                signingCredentials: signingCredentials);
+
+            return tokenOptions;
+        }
+    }
+}
