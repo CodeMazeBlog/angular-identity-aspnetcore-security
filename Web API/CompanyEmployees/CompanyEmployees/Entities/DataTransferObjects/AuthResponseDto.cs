321e3b364ea80580c67f7b5e071eebfb3192d7db
Code Maze <contact@code-maze.com>
2022-05-17 11:47:02 +0200

    remove

--- Web API/CompanyEmployees/CompanyEmployees/Entities/DataTransferObjects/AuthResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CompanyEmployees.Entities.DataTransferObjects
-{
-    public class AuthResponseDto
-    {
-        public bool IsAuthSuccessful { get; set; }
-        public string? ErrorMessage { get; set; }
-        public string? Token { get; set; }
-    }
-}
