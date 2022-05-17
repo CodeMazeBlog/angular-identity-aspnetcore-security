32b8c412e3b583d0ef62d56c76212463062761c2
Code Maze <contact@code-maze.com>
2022-05-17 11:47:22 +0200

    Update to .NET 6 and Angular 13

---
+++ Web API/CompanyEmployees/CompanyEmployees/Entities/DataTransferObjects/ResetPasswordDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompanyEmployees.Entities.DataTransferObjects
+{
+    public class ResetPasswordDto
+    {
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+        
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string? ConfirmPassword { get; set; }
+        
+        public string? Email { get; set; }
+        public string? Token { get; set; }
+    }
+}
