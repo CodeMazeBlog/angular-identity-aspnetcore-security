0cc2bc1e8264e68696ecc25220135f3270b6766f
Code Maze <contact@code-maze.com>
2022-06-13 15:23:13 +0200

    Update both .NET Core and Angular apps. The external auth works again.

---
+++ Web API/CompanyEmployees/CompanyEmployees/Entities/DataTransferObjects/ForgotPasswordDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompanyEmployees.Entities.DataTransferObjects
+{
+    public class ForgotPasswordDto
+    {
+        [Required]
+        [EmailAddress]
+        public string ?Email { get; set; }
+        
+        [Required]
+        public string? ClientURI { get; set; }
+    }
+}
---
+++ Web API/CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,32 @@
+﻿using CompanyEmployees.Contracts;
+using Microsoft.EntityFrameworkCore;
+using CompanyEmployees.Repository;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<RepositoryContext>(opts =>
+                opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), b => b.MigrationsAssembly("CompanyEmployees")));
+
+        public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+           services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+    }
+}
