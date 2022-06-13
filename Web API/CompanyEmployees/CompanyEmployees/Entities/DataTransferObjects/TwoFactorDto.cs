a233170fd29971d2c2f876acbdd9455237d85372
Code Maze <contact@code-maze.com>
2022-06-13 15:23:13 +0200

    Update both .NET Core and Angular apps. The external auth works again.

---
+++ Web API/CompanyEmployees/CompanyEmployees/Entities/DataTransferObjects/TwoFactorDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompanyEmployees.Entities.DataTransferObjects
+{
+    public class TwoFactorDto
+    {
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? Provider { get; set; }
+        [Required]
+        public string? Token { get; set; }
+    }
+}
